609c2c23ff88ae8bbe816077dbcd6025ed917276
Alvaro del Castillo San Felix <adelcastillo@thingso2.com>
2020-06-22 23:58:09 +0200

    WIP: create->transform->reder

---
+++ tests/base.py
@@ -0,0 +1,38 @@
+# Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+# Author (©): Alvaro del Castillo
+
+import logging
+import sys
+import unittest
+
+import mcpi
+
+from mcthings.renderers.raspberry_pi import _Server
+
+from mcthings.renderers.raspberry_pi import RaspberryPi
+from mcthings.world import World
+
+
+class TestBaseThing(unittest.TestCase):
+    """ Integration tests for McThings """
+
+    BUILDER_NAME = "ElasticExplorer"
+
+    MC_SEVER_HOST = "localhost"
+    MC_SEVER_PORT = 4711
+
+    @classmethod
+    def setUpClass(cls):
+        cls.renderer = RaspberryPi(cls.MC_SEVER_HOST, cls.MC_SEVER_PORT)
+
+        try:
+            # TODO: remove this legacy code
+            World.connect(_Server(cls.MC_SEVER_HOST, cls.MC_SEVER_PORT))
+        except mcpi.connection.RequestError:
+            logging.error("Can't connect to Minecraft server " + cls.MC_SEVER_HOST)
+            sys.exit(1)
+
+    def setUp(self):
+        server = self.renderer.server
+        self.pos = server.mc.entity.getTilePos(server.mc.getPlayerEntityId(self.BUILDER_NAME))
+
--- tests/test_u_shaped_stairs.py
+++ tests/test_u_shaped_stairs.py
@@ -2,11 +2,12 @@ import sys
 
 import mcpi.block
 import mcpi.minecraft
+from mcthings.renderers.raspberry_pi import RaspberryPi
 
 from mcthings.server import Server
 from mcthings.world import World
 
-from mcthings_extra.stairs_snail import StairsSnail
+from mcthings_extra.u_shaped_stairs import UShapedStairs
 
 BUILDER_NAME = "ElasticExplorer"
 
@@ -16,13 +17,13 @@ MC_SEVER_PORT = 4711
 
 def main():
     try:
-        World.connect(Server(MC_SEVER_HOST, MC_SEVER_PORT))
+        renderer = RaspberryPi(MC_SEVER_HOST, MC_SEVER_PORT)
 
-        World.server.postToChat("Building stairs")
+        renderer.server._mc.postToChat("Building stairs")
         pos = World.server.entity.getTilePos(World.server.getPlayerEntityId(BUILDER_NAME))
         pos.z -= 1
 
-        stairs = StairsSnail(pos)
+        stairs = UShapedStairs(pos)
         stairs.sections = 4
         stairs.steps = 2
         stairs.width = 3
@@ -30,7 +31,7 @@ def main():
         stairs.build()
 
         World.server.entity.setTilePos(World.server.getPlayerEntityId(BUILDER_NAME),
-                                    stairs.end_position)
+                                       stairs.end_position)
 
     except mcpi.connection.RequestError:
         print("Can't connect to Minecraft server " + MC_SEVER_HOST)
